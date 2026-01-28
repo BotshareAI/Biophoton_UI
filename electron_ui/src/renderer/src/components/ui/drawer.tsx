dcfdb4bb91f8eac3623fbfa0a3ffcfd2d6fbd27a
sheikhhuzaifa <ht096433@gmail.com>
2026-01-28 19:10:27 +0500

    feat: button set

--- electron_ui/src/renderer/src/components/ui/drawer.tsx
+++ electron_ui/src/renderer/src/components/ui/drawer.tsx
@@ -59,7 +59,8 @@ function DrawerContent({
           // 'data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t',
           // 'data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border-l data-[vaul-drawer-direction=right]:sm:max-w-sm',
           // 'data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:border-r data-[vaul-drawer-direction=left]:sm:max-w-sm',
-          'bottom-0 left-64 w-[calc(100%-16rem)]',
+          // On small/touch screens use full width; on larger screens respect left sidebar offset
+          'bottom-0 left-0 w-full md:left-64 md:w-[calc(100%-16rem)] max-h-[90vh]',
           className
         )}
         {...props}
